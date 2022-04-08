56d269d8fd443512fc6f6ecfc899e8462867936e
tandy <tandy1000@cock.li>
2022-04-08 08:30:43 +0100

    remove musicbrainz related extensions, make JSPFPlaylist and JSPFTrack RootObjs

--- jspf.nim
+++ jspf.nim
@@ -1,57 +1,34 @@
 import std/options
 
 type
-  JSPFObject* = ref object of RootObj
-    title*, creator*, annotation*, info*, image*: Option[string]
-
   JSPF* = ref object
     playlist*: JSPFPlaylist
 
-  JSPFPlaylist* = ref object of JSPFObject
-    extension*: Option[PlaylistExtension]
+  JSPFPlaylist* = ref object of RootObj
+    title*, creator*, annotation*, info*, image*: Option[string]
     track*: seq[JSPFTrack]
     date*, license*: Option[string]
     meta*, attribution*, link*: Option[seq[JSPFAttributes]]
     identifier*, location*: Option[string]
 
-  JSPFTrack* = ref object of JSPFObject
+  JSPFTrack* = ref object of RootObj
+    title*, creator*, annotation*, info*, image*: Option[string]
     trackNum*, duration*: Option[int]
     album*: Option[string]
     identifier*, location*: Option[string]
     meta*, link*: Option[JSPFAttributes]
-    extension*: TrackExtension
 
   JSPFAttributes* = ref object
     rel*, content*: string
-
-  PlaylistExtension* = ref object
-    playlistExtensionUrl*: PlaylistExtensionObject
-
-  PlaylistExtensionObject* = ref object
-    creator*, createdFor*: string
-    collaborators*: seq[string]
-    copiedFrom*, lastModifiedAt*: Option[string]
-    copiedFromDeleted*: Option[bool]
-    public*: string
-
-  TrackExtension* = ref object
-    trackExtensionUrl*: TrackExtensionObject
-
-  TrackExtensionObject* = ref object
-    releaseIdentifier*: string
-    addedBy*, addedAt*: Option[string]
-    artistMbids*: seq[string]
 
 func newJSPF*(playlist: JSPFPlaylist): JSPF =
   result = JSPF(playlist: playlist)
 
 func newJSPFPlaylist*(
     track: seq[JSPFTrack],
-    extension: Option[PlaylistExtension] = none(PlaylistExtension),
-    date, license, identifier, location: Option[string] = none(string),
+    title, creator, annotation, info, image, date, license, identifier, location: Option[string] = none(string),
     meta, attribution, link: Option[seq[JSPFAttributes]] = none(seq[JSPFAttributes])): JSPFPlaylist =
   result = JSPFPlaylist(
-    extension: extension,
     track: track,
     date: date,
     license: license,
@@ -83,35 +60,3 @@ func newJSPFTrack*(
 
 func newJSPFAttributes*(rel, content: string): JSPFAttributes =
   result = JSPFAttributes(rel: rel, content: content)
-
-func newPlaylistExtension*(playlistExtensionUrl: PlaylistExtensionObject): PlaylistExtension =
-  result = PlaylistExtension(playlistExtensionUrl: playlistExtensionUrl)
-
-func newPlaylistExtensionObject*(
-  creator, createdFor, public: string,
-  collaborators: seq[string],
-  copiedFrom, lastModifiedAt: Option[string] = none(string),
-  copiedFromDeleted: Option[bool] = none(bool)): PlaylistExtensionObject =
-  result = PlaylistExtensionObject(
-    creator: creator,
-    createdFor: createdFor,
-    collaborators: collaborators,
-    copiedFrom: copiedFrom,
-    lastModifiedAt: lastModifiedAt,
-    copiedFromDeleted: copiedFromDeleted,
-    public: public
-  )
-
-func newTrackExtension*(trackExtensionUrl: TrackExtensionObject): TrackExtension =
-  result = TrackExtension(trackExtensionUrl: trackExtensionUrl)
-
-func newTrackExtensionObject*(
-  releaseIdentifier: string,
-  artistMbids: seq[string],
-  addedBy, addedAt: Option[string] = none(string)): TrackExtensionObject =
-  result = TrackExtensionObject(
-    releaseIdentifier: releaseIdentifier,
-    addedBy: addedBy,
-    addedAt: addedAt,
-    artistMbids: artistMbids
-  )
